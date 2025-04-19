81417f93abbae576f0fea609ab57cb1a95f78c32
Malakin1116 <malyakinabogdan@gmail.com>
2025-04-19 11:43:41 +0300

    cors

--- src/server.js
+++ src/server.js
@@ -19,7 +19,28 @@ export const setupServer = async () => {
 
   await initMongoConnection();
 
-  app.use(cors());
+  // Налаштування CORS
+  app.use(
+    cors({
+      origin: 'http://localhost:5173', // Дозволяємо фронтенд
+      credentials: true, // Дозволяємо cookies
+      methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'], // Дозволені методи
+      allowedHeaders: ['Content-Type', 'Authorization'], // Дозволені заголовки
+    })
+  );
+
+  // Обробка preflight запитів (OPTIONS)
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+    next();
+  });
+
   app.use(express.json());
   app.use(cookieParser());
   app.use(logger);
