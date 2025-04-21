3e271fe06556b9ab2e0d7f50a622f29c1bff9589
Malakin1116 <malyakinabogdan@gmail.com>
2025-04-21 14:11:11 +0300

    secur

--- src/server.js
+++ src/server.js
@@ -22,16 +22,16 @@ export const setupServer = async () => {
   // Налаштування CORS
   app.use(
     cors({
-      origin: 'http://localhost:5173', // Дозволяємо фронтенд
-      credentials: true, // Дозволяємо cookies
-      methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'], // Дозволені методи
-      allowedHeaders: ['Content-Type', 'Authorization'], // Дозволені заголовки
+      origin: ['http://localhost:5173', 'https://localhost:5173'], // Дозволяємо і HTTP, і HTTPS
+      credentials: true,
+      methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
     })
   );
 
   // Обробка preflight запитів (OPTIONS)
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Origin', req.headers.origin); // Динамічно дозволяємо origin
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
